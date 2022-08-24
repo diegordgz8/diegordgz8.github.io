949ab64a6dda581c83f00cc176004ee8a1cf0f82
Diego A. Rodríguez <diegordgz8@outlook.com>
2022-08-23 23:13:47 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "theme-1" : "#17f9ff",
+        "theme-2" : "#00b4fc",
+        "theme-3" : "#005bc5",
+        "theme-4" : "#012677",
+        "theme-5" : "#001449",
+      }
+    },
+  },
+  plugins: [],
+}
